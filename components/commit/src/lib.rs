e7ff935362fa83e3c0444eeba221fb6371603edb
David Komer <6406986+dakom@users.noreply.github.com>
2025-02-14 16:19:28 +0200

    small updates

--- components/commit/src/lib.rs
+++ components/commit/src/lib.rs
@@ -4,6 +4,7 @@ mod telegram;
 use alloy_sol_macro::sol;
 use bindings::{
     export,
+    host::{self, LogLevel},
     wavs::worker::layer_types::{TriggerData, TriggerDataEthContractEvent},
     Guest, TriggerAction,
 };
@@ -22,6 +23,16 @@ impl Guest for Component {
                 if event.operator_id == get_operator_id()? {
                     block_on(async move {
                         let bot = TelegramBot::new().map_err(|e| e.to_string())?;
+                        let bot_name = bot.get_me().await.map_err(|e| e.to_string())?.first_name;
+
+                        host::log(
+                            LogLevel::Info,
+                            &format!(
+                                "Sending message from bot {}, operator {}",
+                                bot_name, event.operator_id
+                            ),
+                        );
+
                         bot.send_message(event.message)
                             .await
                             .map_err(|e| e.to_string())
